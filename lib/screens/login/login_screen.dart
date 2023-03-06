50ea273b0f96d1e5daea00dae4e8a4824a0745be
VoThanhPhi666 <87436394+VoThanhPhi666@users.noreply.github.com>
2023-03-07 05:17:05 +0700

    cong viec #36356: fix issue keyboard overlay login button

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -6,11 +6,10 @@ import 'package:get/get.dart';
 import 'package:gen_crm/screens/login/index.dart';
 import 'package:gen_crm/src/src_index.dart';
 import 'package:gen_crm/widgets/widgets.dart';
-
 import '../../widgets/rounder_bootom_appbar.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 
 class LoginScreen extends StatefulWidget {
-
   LoginScreen({Key? key}) : super(key: key);
 
   @override
@@ -18,64 +17,81 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   String? tokenFirebase;
-
+  ScrollController scrollController = ScrollController();
   @override
   void initState() {
     // fireBase();
+    KeyboardVisibilityController().onChange.listen((visible) {
+      if (visible) {
+        scrollController.jumpTo(150);
+      } else {
+        scrollController.jumpTo(0);
+      }
+    });
+
     super.initState();
   }
 
-
-
-  reload(){
-    setState((){
-
-    });
+  reload() {
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       drawerEnableOpenDragGesture: false,
       body: DoubleBackToCloseApp(
         snackBar: SnackBar(
-          content: Text(MESSAGES.BACK_TO_EXIT, style: AppStyle.DEFAULT_16.copyWith(color: COLORS.WHITE),),
+          content: Text(
+            MESSAGES.BACK_TO_EXIT,
+            style: AppStyle.DEFAULT_16.copyWith(color: COLORS.WHITE),
+          ),
         ),
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              WidgetTouchHideKeyBoard(
-                child: Column(
-                  children: [
-                    RoundedAppBar(),
-                    SingleChildScrollView(
-                      child: Container(
-                        child: Column(
-                          children: [
-                            AppValue.vSpaceMedium,
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text("Đăng nhập",style: TextStyle(fontFamily: "Quicksand", fontWeight: FontWeight.bold,fontSize: 20),)
-                              ],
-                            ),
-                            AppValue.vSpaceMedium,
-                            WidgetLoginForm(reload: reload),
-                          ],
+          physics: ScrollPhysics(),
+          controller: scrollController,
+          child: Padding(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: Column(
+              children: [
+                WidgetTouchHideKeyBoard(
+                  child: Column(
+                    children: [
+                      RoundedAppBar(),
+                      SingleChildScrollView(
+                        child: Container(
+                          child: Column(
+                            children: [
+                              AppValue.vSpaceMedium,
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "Đăng nhập",
+                                    style: TextStyle(
+                                        fontFamily: "Quicksand",
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20),
+                                  )
+                                ],
+                              ),
+                              AppValue.vSpaceMedium,
+                              WidgetLoginForm(reload: reload),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
   }
-
-
 }
