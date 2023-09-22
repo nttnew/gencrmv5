fe5481d24bbe289477b433ab78184a5ee59fdfe4
doanhdd0210 <doanhdd0210@gmail.com>
2023-09-22 14:13:56 +0700

    version 2.1.2

--- lib/screens/menu/menu_left/menu_drawer/main_drawer.dart
+++ lib/screens/menu/menu_left/menu_drawer/main_drawer.dart
@@ -369,7 +369,7 @@ class _MainDrawerState extends State<MainDrawer> {
           AppValue.vSpaceTiny,
           Center(
             child: WidgetText(
-              title: 'Version: 2.1.1',
+              title: 'Version: 2.1.2',
               style: AppStyle.DEFAULT_16.copyWith(
                   fontFamily: 'Montserrat', fontWeight: FontWeight.w400),
             ),
