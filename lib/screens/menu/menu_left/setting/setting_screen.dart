9ab916bda4c2b55a8131c35660c707c4bda8aa29
doanhdd0210 <doanhdd0210@gmail.com>
2024-08-02 17:44:50 +0700

    v:2.7.7 Update UI

--- lib/screens/menu/menu_left/setting/setting_screen.dart
+++ lib/screens/menu/menu_left/setting/setting_screen.dart
@@ -315,7 +315,7 @@ class _SettingScreenState extends State<SettingScreen> {
           AppValue.vSpaceTiny,
           Center(
             child: WidgetText(
-              title: 'Version: 2.4.5',
+              title: 'Version: 2.4.6',
               style: AppStyle.DEFAULT_16.copyWith(
                 fontFamily: 'Montserrat',
                 fontWeight: FontWeight.w400,
