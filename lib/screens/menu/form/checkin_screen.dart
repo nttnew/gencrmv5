d4b10f3948a7ad2c26840f3b4b0eba6f1430a8ac
doanhdd0210 <doanhdd0210@gmail.com>
2024-08-08 14:18:25 +0700

    v:2.7.8 Update UI + fix bug

--- lib/screens/menu/form/checkin_screen.dart
+++ lib/screens/menu/form/checkin_screen.dart
@@ -7,12 +7,12 @@ import 'package:gen_crm/src/models/model_generator/add_customer.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:widget_and_text_animator/widget_and_text_animator.dart';
 import '../../../l10n/key_text.dart';
 import '../../../src/app_const.dart';
 import '../../../src/src_index.dart';
 import '../../../widgets/appbar_base.dart';
 import '../../../widgets/btn_save.dart';
-import '../../../widgets/loading_api.dart';
 import '../../../widgets/location_base.dart';
 import '../../../widgets/widget_text.dart';
 
@@ -32,18 +32,17 @@ class _CheckInScreenState extends State<CheckInScreen> {
   Position? position;
   late final CheckInBloc _blocCheckIn;
 
-  getNameLocation() async {
-    Loading().showLoading();
+  getNameLocation({bool isLoadingNow = false}) async {
+    if(isLoadingNow){
+      nameLocation.add(LOADING);
+    }
     position = await determinePosition(context);
     if (position != null) {
-      nameLocation.add(LOADING);
       try {
         final location = await getLocationName(
             position?.latitude ?? 0, position?.longitude ?? 0);
         nameLocation.add(location);
-        Loading().popLoading();
       } catch (e) {
-        Loading().popLoading();
         ShowDialogCustom.showDialogBase(
           title: getT(KeyT.notification),
           content: getT(KeyT.an_error_occurred),
@@ -73,7 +72,6 @@ class _CheckInScreenState extends State<CheckInScreen> {
       bloc: _blocCheckIn,
       listener: (BuildContext context, state) {
         if (state is SuccessCheckInState) {
-          Loading().popLoading();
           ShowDialogCustom.showDialogBase(
             title: getT(KeyT.notification),
             content: getT(KeyT.new_data_added_successfully),
@@ -84,7 +82,6 @@ class _CheckInScreenState extends State<CheckInScreen> {
             },
           );
         } else if (state is ErrorCheckInState) {
-          Loading().popLoading();
           ShowDialogCustom.showDialogBase(
             title: getT(KeyT.notification),
             content: state.msg,
@@ -104,148 +101,141 @@ class _CheckInScreenState extends State<CheckInScreen> {
               final location = snapshot.data ?? '';
               return Column(
                 crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          child: Row(
-                            children: [
-                              WidgetText(
-                                title: getT(KeyT.your_position),
-                                style: AppStyle.DEFAULT_18_BOLD,
-                              ),
-                              WidgetText(
-                                title: '*',
-                                style: AppStyle.DEFAULT_18_BOLD.copyWith(
-                                  color: COLORS.RED,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        if (location != '') ...[
-                          SizedBox(
-                            height: 16,
-                          ),
-                          Row(
-                            children: [
-                              Container(
-                                height: 16,
-                                width: 16,
-                                child: SvgPicture.asset(
-                                  ICONS.IC_LOCATION_SVG,
-                                  fit: BoxFit.contain,
-                                ),
-                              ),
-                              SizedBox(
-                                width: 4,
-                              ),
-                              location != LOADING
-                                  ? Expanded(
-                                      child: WidgetText(
-                                          title: location,
-                                          style: AppStyle.DEFAULT_14W600),
-                                    )
-                                  : SizedBox(
-                                      height: 12,
-                                      width: 12,
-                                      child: CircularProgressIndicator(
-                                        strokeWidth: 1.4,
-                                      ),
-                                    ),
-                            ],
-                          ),
-                        ],
-                        AppValue.vSpaceSmall,
-                        if (location == '')
-                          _buttonCheck(
-                            isCheckIn
-                                ? getT(KeyT.check_in)
-                                : getT(KeyT.check_out),
-                            () async {
-                              await getNameLocation();
-                            },
-                          ),
-                        if (location != '')
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              _buttonCheck(
-                                isCheckIn
-                                    ? getT(KeyT.check_in_again)
-                                    : getT(KeyT.check_out_again),
-                                () async {
-                                  await getNameLocation();
-                                },
-                                backGround: COLORS.TEXT_BLUE_BOLD,
-                              ),
-                              AppValue.hSpace10,
-                              _buttonCheck(
-                                getT(KeyT.delete),
-                                () {
-                                  nameLocation.add('');
-                                },
-                                backGround: COLORS.RED,
-                              ),
-                            ],
-                          ),
-                        AppValue.vSpaceSmall,
-                        if (location != '') ...[
-                          Container(
-                            child: TextFormField(
-                              textCapitalization: TextCapitalization.sentences,
-                              controller: controllerNote,
-                              style: AppStyle.DEFAULT_14_BOLD,
-                              onChanged: (text) {},
-                              decoration: InputDecoration(
-                                contentPadding: paddingBaseForm,
-                                label: WidgetLabelPo(
-                                  data: CustomerIndividualItemData.two(
-                                    field_label: getT(KeyT.location),
-                                    field_require: 1,
-                                  ),
-                                ),
-                                border: OutlineInputBorder(),
-                                isDense: true,
+                  Column(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        child: Row(
+                          children: [
+                            WidgetText(
+                              title: getT(KeyT.your_position),
+                              style: AppStyle.DEFAULT_18_BOLD,
+                            ),
+                            WidgetText(
+                              title: '*',
+                              style: AppStyle.DEFAULT_18_BOLD.copyWith(
+                                color: COLORS.RED,
                               ),
                             ),
-                          ),
-                        ],
-                        AppValue.vSpaceSmall,
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        ButtonSave(
-                          onTap: () {
-                            if (controllerNote.text.trim() == '') {
-                              ShowDialogCustom.showDialogBase(
-                                title: getT(KeyT.notification),
-                                content:
-                                    getT(KeyT.please_enter_all_required_fields),
-                              );
-                            } else {
-                              _blocCheckIn.add(
-                                SaveCheckIn(
-                                  '${position?.longitude ?? ''}',
-                                  '${position?.latitude ?? ''}',
-                                  controllerNote.text,
-                                  id,
-                                  module,
-                                  type,
-                                ),
-                              );
-                            }
-                          },
+                          ],
+                        ),
+                      ),
+                      if (location != '') ...[
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              height: 16,
+                              width: 16,
+                              child: SvgPicture.asset(
+                                ICONS.IC_LOCATION_SVG,
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 4,
+                            ),
+                            location != LOADING
+                                ? Expanded(
+                                    child: TextAnimator(
+                                      location,
+                                      style: AppStyle.DEFAULT_14W600.copyWith(
+                                        fontStyle: FontStyle.italic,
+                                      ),
+                                      atRestEffect: WidgetRestingEffects.slide(
+                                        numberOfPlays: 1,
+                                      ),
+                                    ),
+                                  )
+                                : SizedBox(
+                                    height: 12,
+                                    width: 12,
+                                    child: CircularProgressIndicator(
+                                      strokeWidth: 1.4,
+                                    ),
+                                  ),
+                          ],
                         ),
                       ],
-                    ),
+                      AppValue.vSpaceSmall,
+                      if (location == '')
+                        _buttonCheck(
+                          isCheckIn
+                              ? getT(KeyT.check_in)
+                              : getT(KeyT.check_out),
+                          () async {
+                            await getNameLocation(isLoadingNow: true);
+                          },
+                        ),
+                      if (location != '')
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            _buttonCheck(
+                              isCheckIn
+                                  ? getT(KeyT.check_in_again)
+                                  : getT(KeyT.check_out_again),
+                              () async {
+                                await getNameLocation(isLoadingNow: true);
+                              },
+                              backGround: COLORS.TEXT_BLUE_BOLD,
+                            ),
+                            AppValue.hSpace10,
+                            _buttonCheck(
+                              getT(KeyT.delete),
+                              () {
+                                nameLocation.add('');
+                              },
+                              backGround: COLORS.RED,
+                            ),
+                          ],
+                        ),
+                      AppValue.vSpaceSmall,
+                      if (location != '')
+                        TextFormField(
+                          textCapitalization: TextCapitalization.sentences,
+                          controller: controllerNote,
+                          style: AppStyle.DEFAULT_14_BOLD,
+                          onChanged: (text) {},
+                          decoration: InputDecoration(
+                            contentPadding: paddingBaseForm,
+                            label: WidgetLabel(
+                              CustomerIndividualItemData.two(
+                                field_label: getT(KeyT.location),
+                                field_require: 1,
+                              ),
+                            ),
+                            border: OutlineInputBorder(),
+                            isDense: true,
+                          ),
+                        ),
+                      AppValue.vSpaceSmall,
+                    ],
+                  ),
+                  ButtonSave(
+                    onTap: () {
+                      if (controllerNote.text.trim() == '') {
+                        ShowDialogCustom.showDialogBase(
+                          title: getT(KeyT.notification),
+                          content: getT(KeyT.please_enter_all_required_fields),
+                        );
+                      } else {
+                        _blocCheckIn.add(
+                          SaveCheckIn(
+                            '${position?.longitude ?? ''}',
+                            '${position?.latitude ?? ''}',
+                            controllerNote.text,
+                            id,
+                            module,
+                            type,
+                          ),
+                        );
+                      }
+                    },
                   ),
                 ],
               );
