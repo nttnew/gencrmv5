fc6e35a877e915b217cedd360a8f18e175b0758a
doanhdd0210 <doanhdd0210@gmail.com>
2024-10-07 14:05:05 +0700

    refactor code

--- lib/src/base.dart
+++ lib/src/base.dart
@@ -206,7 +206,7 @@ class BASE_URL {
       'modules/genmobile2/helper/getlicensePlateFromImage';
 
   static const ENV = 'assets/.env';
-  static const int connectionTimeout = 30000;
+  static const int connectionTimeout = 60000;
   static const int receiveTimeout = 60000;
   static const content_type = 'Content-Type';
   static const language = 'language';
